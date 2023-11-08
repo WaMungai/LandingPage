d4c4c1df97e47ea2bcd5fc141f1681e31febd8bc
The Passionate Programmer <wamungai10@gmail.com>
2023-11-08 11:50:23 -0800

    Refactor customer review  section styling

--- src/components/CustomerReview.jsx
+++ src/components/CustomerReview.jsx
@@ -4,64 +4,72 @@ import IMAGES from '../images/Images';
 function CustomerReview() {
   return (
     <>
-   <div className="bg-teal-500">
-  <div className="items-start flex w-[632px] max-w-full grow flex-col mt-20 self-start max-md:mt-10">
-    <div className="text-white text-5xl font-bold self-start max-md:max-w-full max-md:text-4xl">
-      Our customer reviews speak for themselves
-    </div>
-    <div className="items-start flex w-[297px] max-w-full justify-between gap-5 mt-2.5 self-start">
-      <div className="text-yellow-400 text-sm self-center grow shrink basis-auto my-auto">
-        Read real customer reviews on G2
-      </div>
-    </div>
-  </div>
-  <div className="flex flex-col pt-60 max-md:pt-24">
-    <div className="shadow-lg backdrop-blur-[100px] self-stretch w-full pt-0 mt-60 pl-24 rounded-2xl border-[1.772px] border-solid border-white max-md:max-w-full max-md:mt-10 max-md:pl-5">
-      <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-        <div className="flex flex-col items-stretch w-[51%] max-md:w-full max-md:ml-0">
-          <div className="items-start flex flex-col my-auto max-md:max-w-full max-md:mt-10">
-            <div className="items-start self-stretch flex flex-col max-md:max-w-full">
-              <div className="text-white text-5xl font-bold self-stretch max-md:max-w-full max-md:text-4xl">
-                Let us be part of your growth journey
-              </div>
-              <div className="text-white text-base leading-6 self-stretch mt-7 max-md:max-w-full">
-                Our ultimate vision is to bring producers closer to their
-                consumers through retailers like you. We believe technology is
-                unlocking the potential of retail, and we’d love to partner
-                with you and move your business forward.
-              </div>
-            </div>
-            <div className="justify-center items-start self-stretch bg-white bg-opacity-20 flex grow flex-col mt-8 pl-8 pr-20 py-8 rounded-md max-md:max-w-full max-md:px-5">
-              <div className="text-white text-2xl font-bold leading-8 w-[346px] max-w-full self-start">
-                Do own or think of setting up a retail outlet? Talk to us.
-              </div>
-              <div className="items-start flex w-[106px] max-w-full gap-2.5 mt-5 self-start">
-                <div className="text-teal-700 text-sm self-center my-auto">
-                  Book a call
+      <div className="bg-teal-500 pt-24">
+        <div className="DecriptionWrapper px-20">
+          <div className="self-stretch w-full max-md:max-w-full pt-24 pb-12">
+            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0 ">
+              <div className="flex flex-col items-stretch w-[53%] max-md:w-full max-md:ml-0 pl-24">
+                <div className="text-5xl font-bold flex-1 max-md:max-w-full max-md:text-3xl max-md:mt-10 text-white">
+                  <h1>Our customers reviews speak for themselves</h1>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex flex-col items-stretch w-[49%] ml-5 max-md:w-full max-md:ml-0">
-          <div className="grow max-md:max-w-full max-md:mt-0">
-            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-              <div className="flex flex-col items-stretch w-9/12 max-md:w-full max-md:ml-0">
-                <img
-                  loading="lazy"
-                  src={IMAGES.image10}
-                  alt={"logo"}
-                  className="aspect-[0.6] object-contain object-center w-full overflow-hidden grow max-md:max-w-full"
-                />
+
+        <div className="mx-auto px-24">
+          <div className="bg-white bg-white/40 h-[575px] overflow-hidden rounded-lg">
+            <div className="grid grid-cols-2">
+              <div className="LeftWrapper px-16">
+                {/* Left */}
+                <div className="self-stretch w-full max-md:max-w-full pt-24 pb-12">
+                  <div className="flex flex-col items-stretch max-md:w-full max-md:ml-0">
+                    <div className="items-start flex flex-col my-auto max-md:max-w-full max-md:mt-10">
+                      <div className="items-start self-stretch flex flex-col max-md:max-w-full">
+                        <div className="text-white text-5xl font-bold self-stretch max-md:max-w-full max-md:text-4xl">
+                          Let us be part of your growth journey
+                        </div>
+                        <div className="text-white text-base leading-6 self-stretch mt-7 max-md:max-w-full">
+                          Our ultimate vision is to bring producers closer to
+                          their consumers through retailers like you. We believe
+                          technology is unlocking the potential of retail, and
+                          we’d love to partner with you and move your business
+                          forward.
+                        </div>
+                      </div>
+                      <div className="justify-center items-start self-stretch bg-white bg-opacity-20 flex grow flex-col mt-8 pl-8 pr-20 py-8 rounded-md max-md:max-w-full max-md:px-5">
+                        <div className="text-white text-2xl font-bold leading-8 w-[346px] max-w-full self-start">
+                          Do own or think of setting up a retail outlet? Talk to
+                          us.
+                        </div>
+                        <div className="items-start flex w-[106px] max-w-full gap-2.5 mt-5 self-start">
+                          <div className="text-teal-700 text-sm self-center my-auto">
+                            Book a call
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Right */}
+              <div className="RightWrapper">
+                <div className="relative flex flex-col items-stretch ml-5 max-md:w-full max-md:ml-0 z-50">
+                  <img
+                    loading="lazy"
+                    src={IMAGES.image10}
+                    alt={"logo"}
+                    className=" absolute aspect-[0.6] object-contain object-center w-full overflow-hidden grow max-md:max-w-full -mt-64"
+                  />
+                </div>
+
+               
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </div>
-</div>
-    </> 
+    </>
   );
 }
 
--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -4,129 +4,135 @@ import IMAGES from '../images/Images'
 function Resources() {
   return (
     <>
-    <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-    <div className="flex flex-col items-stretch w-[55%] max-md:w-full max-md:ml-0">
-      <div className="text-black text-5xl font-bold w-[433px] max-w-[433px] max-md:max-w-full max-md:text-4xl max-md:mt-10">
-        Resources for your business
-      </div>
-    </div>
-    <div className="flex flex-col items-stretch w-[45%] ml-5 max-md:w-full max-md:ml-0">
-      <div className="text-black text-lg leading-8 w-[389px] max-w-[389px] max-md:mt-10">
-        Discover up-and-coming trends, access free guides, and learn how
-        business owners like you use Mzoori’s POS system to level up.
-      </div>
-    </div>
-  </div>
-  <div className="flex-col overflow-hidden self-stretch relative flex min-h-[1079px] w-full pt-0 pb-0 mt-32 px-20 max-md:max-w-full max-md:mt-10 max-md:px-5">
-  <img
-    loading="lazy"
-    srcSet="..."
-    className="absolute z-[-1] h-full w-full object-cover object-center inset-0"
-  />
-  
-  <div className="relative self-center z-[1] flex w-full max-w-[1180px] flex-col max-md:max-w-full " >
-    <div className="self-center flex w-full max-w-[980px] flex-col max-md:max-w-full">
-      <div className="self-center w-full max-md:max-w-full">
-        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
-            <div className="items-start border shadow-lg bg-white flex w-full max-w-[312px] grow flex-col mx-auto pl-4 pr-3.5 py-4 rounded-xl border-solid border-zinc-100 max-md:mt-6">
-              <img
-                loading="lazy"
-                src={IMAGES.image11}
-                alt={"logo"}
-                className="aspect-square object-contain object-center w-[60px] justify-center items-center overflow-hidden max-w-full self-start"
-              />
-              <div className="text-black text-2xl font-bold mt-7 self-start">
-                SeaVees
-              </div>
-              <div className="text-neutral-500 text-base leading-7 mt-7 self-start">
-                SeaVees employs POS Go for a more flexible and untethered
-                checkout experience
-                <br />
-              </div>
-              <div className="items-start flex w-full justify-between gap-5 mt-7 pr-px self-start">
-                <div className="text-teal-500 text-sm font-bold my-auto">
-                  View all Mzoori POS features
+      <div
+        className="bg-teal-100 h-[505px] w-[1360px
+] mr-20"
+      >
+        <div className="DecriptionWrapper px-20">
+          <div className="self-stretch w-full max-md:max-w-full pt-24 pb-12">
+            <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0 ">
+              <div className="flex flex-col items-stretch w-[53%] max-md:w-full max-md:ml-0">
+                <div className="text-black text-6xl font-bold flex-1 max-md:max-w-full max-md:text-4xl max-md:mt-10">
+                  <h1>Resources for your business</h1>
                 </div>
-                <img
-                  loading="lazy"
-                  src={IMAGES.image12}
-                   alt={"logo"}
-                  className="aspect-[0.79] object-contain object-center w-[19px] overflow-hidden self-stretch max-w-full"
-                />
+              </div>
+              <div className="flex flex-col items-stretch w-[47%] ml-5 max-md:w-full max-md:ml-0">
+                <div className="text-black text-2xl font-normal flex-1 max-md:max-w-full max-md:mt-10">
+                  <p>
+                    Discover up-and-coming trends, access free guides, and learn
+                    how business owners like you use Mzoori’s POS system to
+                    level up.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
-          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
-            <div className="items-start border shadow-lg bg-white flex w-full max-w-[312px] grow flex-col mx-auto pl-4 pr-3.5 py-4 rounded-xl border-solid border-zinc-100 max-md:mt-6">
-              <img
-                loading="lazy"
-                src={IMAGES.image11}
-                alt={"logo"}
-                className="aspect-square object-contain object-center w-[60px] justify-center items-center overflow-hidden max-w-full self-start"
-              />
-              <div className="text-black text-2xl font-bold mt-7 self-start">
-                Lively
-              </div>
-              <div className="text-neutral-500 text-base leading-7 mt-7 self-start">
-                Lively’s omnichannel approach increased average order
-                value by 80%
-                <br />
-              </div>
-              <div className="items-start flex w-full justify-between gap-5 mt-7 pr-px self-start">
-                <div className="text-teal-500 text-sm font-bold my-auto">
-                  View all Mzoori POS features
+        </div>
+
+        <div className="CardWrapper px-20">
+          <div className="flex-col overflow-hidden self-stretch relative flex  w-full pt-0 pb-0 px-20 max-md:max-w-full max-md:mt-10 max-md:px-5">
+          
+
+            <div className="relative z-[1] flex w-full max-w-[1180px] flex-col max-md:max-w-full ">
+              <div className="self-center flex w-full max-w-[980px] flex-col max-md:max-w-full">
+                <div className="self-center w-full max-md:max-w-full">
+                  <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+                    <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+                      <div className="items-start border shadow-lg bg-white flex w-full max-w-[312px] grow flex-col mx-auto pl-4 pr-3.5 py-4 rounded-xl border-solid border-zinc-100 max-md:mt-6">
+                        <img
+                          loading="lazy"
+                          src={IMAGES.image11}
+                          alt={"logo"}
+                          className="aspect-square object-contain object-center w-[60px] justify-center items-center overflow-hidden max-w-full self-start"
+                        />
+                        <div className="text-black text-2xl font-bold mt-7 self-start">
+                          SeaVees
+                        </div>
+                        <div className="text-neutral-500 text-base leading-7 mt-7 self-start">
+                          SeaVees employs POS Go for a more flexible and
+                          untethered checkout experience
+                          <br />
+                        </div>
+                        <div className="items-start flex w-full justify-between gap-5 mt-7 pr-px self-start">
+                          <div className=" bottom-0 text-teal-500 text-sm font-bold my-auto">
+                            View all Mzoori POS features
+                          </div>
+                          <img
+                            loading="lazy"
+                            src={IMAGES.image12}
+                            alt={"logo"}
+                            className="aspect-[0.79] object-contain object-center w-[19px] overflow-hidden self-stretch max-w-full"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+                      <div className="items-start border shadow-lg bg-white flex w-full max-w-[312px] grow flex-col mx-auto pl-4 pr-3.5 py-4 rounded-xl border-solid border-zinc-100 max-md:mt-6">
+                        <img
+                          loading="lazy"
+                          src={IMAGES.image11}
+                          alt={"logo"}
+                          className="aspect-square object-contain object-center w-[60px] justify-center items-center overflow-hidden max-w-full self-start"
+                        />
+                        <div className="text-black text-2xl font-bold mt-7 self-start">
+                          Lively
+                        </div>
+                        <div className="text-neutral-500 text-base leading-7 mt-7 self-start">
+                          Lively’s omnichannel approach increased average order
+                          value by 80%
+                          <br />
+                        </div>
+                        <div className="items-start flex w-full justify-between gap-5 mt-7 pr-px self-start">
+                          <div className=" text-teal-500 text-sm font-bold my-auto">
+                            View all Mzoori POS features
+                          </div>
+                          <img
+                            loading="lazy"
+                            src={IMAGES.image12}
+                            alt={"logo"}
+                            className="aspect-[0.79] object-contain object-center w-[19px] overflow-hidden self-stretch max-w-full"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
+                      <div className="items-start border shadow-lg bg-white flex w-full max-w-[312px] grow flex-col mx-auto pl-4 pr-3.5 py-4 rounded-xl border-solid border-zinc-100 max-md:mt-6">
+                        <img
+                          loading="lazy"
+                          src={IMAGES.image11}
+                          alt={"logo"}
+                          className="aspect-square object-contain object-center w-[60px] justify-center items-center overflow-hidden max-w-full self-start"
+                        />
+                        <div className="text-black text-2xl font-bold mt-7 self-start">
+                          Nalata Nalata
+                        </div>
+                        <div className="text-neutral-500 text-base leading-7 mt-7 self-start">
+                          Nalata Nalata used their retail location to drive ~50%
+                          of sales across every channel
+                          <br />
+                        </div>
+                        <div className="items-start flex w-full justify-between gap-5 mt-7 pr-px self-start">
+                          <div className="text-teal-500 text-sm font-bold my-auto">
+                            View all Mzoori POS features
+                          </div>
+                          <img
+                            loading="lazy"
+                            src={IMAGES.image12}
+                            alt={"logo"}
+                            className="aspect-[0.79] object-contain object-center w-[19px] overflow-hidden self-stretch max-w-full"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-                <img
-                  loading="lazy"
-                  src={IMAGES.image12}
-                  alt={"logo"}
-                  className="aspect-[0.79] object-contain object-center w-[19px] overflow-hidden self-stretch max-w-full"
-                />
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col items-stretch w-[33%] ml-5 max-md:w-full max-md:ml-0">
-            <div className="items-start border shadow-lg bg-white flex w-full max-w-[312px] grow flex-col mx-auto pl-4 pr-3.5 py-4 rounded-xl border-solid border-zinc-100 max-md:mt-6">
-              <img
-                loading="lazy"
-                src={IMAGES.image11}
-                alt={"logo"}
-                className="aspect-square object-contain object-center w-[60px] justify-center items-center overflow-hidden max-w-full self-start"
-              />
-              <div className="text-black text-2xl font-bold mt-7 self-start">
-                Nalata Nalata
-              </div>
-              <div className="text-neutral-500 text-base leading-7 mt-7 self-start">
-                Nalata Nalata used their retail location to drive ~50% of
-                sales across every channel
-                <br />
-              </div>
-              <div className="items-start flex w-full justify-between gap-5 mt-7 pr-px self-start">
-                <div className="text-teal-500 text-sm font-bold my-auto">
-                  View all Mzoori POS features
-                </div>
-                <img
-                  loading="lazy"
-                  src={IMAGES.image12}
-                  alt={"logo"}
-                  className="aspect-[0.79] object-contain object-center w-[19px] overflow-hidden self-stretch max-w-full"
-                />
               </div>
             </div>
           </div>
         </div>
       </div>
-      
-    </div>
-   
-  </div>
-</div>
-  </>
-    
- 
-  
-  )
+    </>
+  );
 }
 
 export default Resources
