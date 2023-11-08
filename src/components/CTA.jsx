072f60dd0241ffdea5fb5c61caa58e9b4a3715d3
The Passionate Programmer <wamungai10@gmail.com>
2023-11-08 11:41:45 -0800

    Refactor landing page styling

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -39,6 +39,7 @@ function CTA() {
                 </div>
               </div>
             </div>
+            /*** */
             <div className="self-stretch mt-11 pr-44 max-md:max-w-full max-md:mt-10 max-md:pr-5">
               <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
                 <div className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0">
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -13,33 +13,38 @@ import CarouselContainer from "../components/CarouselContainer";
 
 function LandingPage() {
   return (
-    <div className="bg-white flex flex-col">
-      <HeroSection />
-      <div className="self-center z-[50] flex mt-0 w-full max-w-[1331px] flex-col max-md:max-w-full">
-        <ModernSection />
-        <div className="items-start self-center flex w-full max-w-[1124px] flex-col mt-28 max-md:max-w-full max-md:mt-10">
-          <Specs />
-        </div>
-        <div className="items-center shadow-lg bg-slate-50 self-stretch flex flex-col mt-24 pb-24 rounded-2xl max-md:max-w-full max-md:mt-10">
-          <div className="flex w-[277px] max-w-full items-start gap-0 pr-6 self-start max-md:pr-5">
+    <>
+      <div className="bg-white flex flex-col mx-auto ">
+        <HeroSection />
+        <div className="self-center z-[50] flex mt-0 w-full max-w-[1331px] flex-col max-md:max-w-full">
+          <ModernSection />
+          <div className="items-start self-center flex w-full max-w-[1124px] flex-col mt-28 max-md:max-w-full max-md:mt-10">
+            <Specs />
           </div>
-          <div className="items-start self-center flex w-full max-w-[1233px] flex-col max-md:max-w-full">
-            <CarouselContainer />
-            <div className="self-stretch mt-11 pr-44 max-md:max-w-full max-md:mt-10 max-md:pr-5">
-              <CTA />
+          <div className="items-center shadow-lg bg-slate-50 self-stretch flex flex-col mt-24 pb-24 rounded-2xl max-md:max-w-full max-md:mt-10">
+            <div className="flex w-[277px] max-w-full items-start gap-0 pr-6 self-start max-md:pr-5"></div>
+            <div className="items-start self-center flex w-full max-w-[1233px] flex-col max-md:max-w-full px-24 ">
+              <CarouselContainer />
+              <div className="self-stretch mt-12 max-md:max-w-full max-md:mt-10 max-md:pr-5">
+                <CTA />
+              </div>
             </div>
           </div>
+          <Features />
+          <div className="self-stretch mt-24 pr-56 max-md:max-w-full max-md:mt-10 max-md:pr-5">
+            <PosHardware />
+          </div>
         </div>
-        <Features />
-        <div className="self-stretch mt-24 pr-56 max-md:max-w-full max-md:mt-10 max-md:pr-5">
-          <PosHardware />
+
+        <div className="self-center  flex mt-0 w-full max-w-[1331px] flex-col max-md:max-w-full">
+          <Resources />
+          <CustomerReview />
+          <FAQ />
         </div>
+
+        <Footer />
       </div>
-      <Resources />
-      <CustomerReview />
-      <FAQ />
-      <Footer />
-    </div>
+    </>
   );
 }
 export default LandingPage
